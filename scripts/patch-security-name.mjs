4c236a4c89c0714985d16b37e9d25a0cb54349e4
Logan Allenstein <ljaicsi25@gmail.com>
2026-05-08 10:30:49 -0500

    Delete scripts directory

--- scripts/patch-security-name.mjs
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-const p = new URL("../assets/learn-hub-courses.js", import.meta.url);
-let t = fs.readFileSync(p, "utf8");
-const a = '"id":"security","name":"Security"';
-const b = '"id":"security","name":"Security+"';
-if (!t.includes(a)) throw new Error("pattern not found");
-t = t.replace(a, b);
-fs.writeFileSync(p, t, "utf8");
-console.log("ok");
